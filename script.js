c095c9116e31e63d57aa65be942a24debb79b050
Matheus Oliveira <matheusoliveira18rj@gmail.com>
2022-04-03 21:35:51 -0300

    [Feature] Static files added and HTML structured

---
+++ script.js
@@ -0,0 +1,16 @@
+let thankYouState = `
+    <!-- Rating state end -->
+
+    <!-- Thank you state start -->
+
+    <span>You selected <!-- Add rating here --> out of 5</span>
+
+    <h1>Thank you</h1>
+
+    <p>
+    We appreciate you taking the time to give a rating. If you ever need more support, 
+    don’t hesitate to get in touch!
+    </p>
+
+    <!-- Thank you state end -->
+`
